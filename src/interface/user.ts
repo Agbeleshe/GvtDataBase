7d6f05d15a8f8a22af74586824a83e8d16faa087
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-10-09 18:06:04 +0000

    Initial commit

---
+++ src/interface/user.ts
@@ -0,0 +1,16 @@
+export interface newUserType {
+    fullName: string
+    dateOfBirth: DateConstructor,
+    nationality: string,
+    stateOfOrigin: string,
+    localGovt: string,
+    address: string,
+    phoneNumber: string,
+    email: string,
+    nationalIdentityNumber: number,
+    employmentStatus: string,
+    jobTitle: string,
+    highestEducationLevel: string,
+    password: string,
+    terms: true,
+}
