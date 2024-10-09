629527ad0d87236bc068849bec5f168d8a7c93e5
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-10-09 18:08:55 +0000

    Removing unused code

--- src/routes/NonUserRoutes.tsx
+++ src/routes/NonUserRoutes.tsx
@@ -1,27 +1,25 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
-import CreateUser from "../pages/CreateUser";
 import Login from "../pages/Login";
 
 import { ReactNode } from "react";
 import { AboutUs } from "../pages/AboutUs";
 import { NavBar } from "../components/NavBar";
 import ServiceDetails from "../pages/ServiceDetails";
-// import { useMediaQuery } from "@mantine/hooks";
+import Signup from "../pages/CreateUser";
 
 interface NonUserRoutesProps {
   children: ReactNode;
 }
 
 export const NonUserRoutes: React.FC<NonUserRoutesProps> = ({ children }) => {
-  // const md = useMediaQuery("(max-width: 768px)");
 
   return (
     <>
       <NavBar>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/create-user" element={<CreateUser />} />
+          <Route path="/create-user" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/about" element={<AboutUs />} />
           <Route path="/services" element={<ServiceDetails />} />
@@ -41,8 +39,6 @@ export const NonUserRoutes: React.FC<NonUserRoutesProps> = ({ children }) => {
           }}
         >
           <div id="up"></div>
-          {/* <div id="down"></div>
-          <div id="downMobile"></div> */}
         </div>
       </div>
       {children}
