be77e0d056d00c94ab1ed354bbd419bb3aaff17f
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-10-10 08:33:13 +0000

    Adding notification system

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { Notifications } from "@mantine/notifications"
+import { MantineProvider, } from "@mantine/core";
 import "@mantine/core/styles.css";
 import '@mantine/dates/styles.css';
-import { MantineProvider } from "@mantine/core";
+import '@mantine/notifications/styles.css';
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import AuthContextProvider from "./context/AuthContext.tsx";
+import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <MantineProvider>
     <AuthContextProvider>
       <BrowserRouter>
+        <Notifications />
         <App />
       </BrowserRouter>
     </AuthContextProvider>
